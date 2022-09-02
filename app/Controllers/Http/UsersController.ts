99c4e3f5d4292e8c377a01b021f0f94f775e4fba
akandeseun <akandeseun44@gmail.com>
2022-09-02 15:00:14 +0100

    wip:authentication

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import createUserValidator from 'App/Validators/CreateUserValidator'
+import UserSignInValidator from 'App/Validators/UserSignInValidator'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class UsersController {
   public async createUser({ request, response }: HttpContextContract) {
@@ -9,5 +11,24 @@ export default class UsersController {
 
     const user = await User.create(payload)
     return response.status(201).json({ user })
+  }
+
+  public async authenticateUser({ request, response }: HttpContextContract) {
+    const payload = await request.validate(UserSignInValidator)
+
+    const user = await User.findBy('email', payload.email)
+    if (!user) {
+      return response.status(400).json({
+        msg: 'Incorrect email or password',
+      })
+    }
+    if (!(await Hash.verify(user.password, payload.password))) {
+      return response.status(400).json({
+        msg: 'Incorrect email or password',
+      })
+    }
+    return response.status(200).json({
+      msg: `welcome ${user.username}`,
+    })
   }
 }
--- app/Validators/UserSignInValidator.ts
+++ app/Validators/UserSignInValidator.ts
@@ -5,9 +5,12 @@ export default class UserSignInValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    email: schema.string([rules.required()]),
+    email: schema.string([rules.required(), rules.email()]),
     password: schema.string([rules.required()]),
   })
 
-  public messages: CustomMessages = {}
+  public messages: CustomMessages = {
+    required: '{{ field }} is required',
+    email: 'Input a valid email',
+  }
 }
