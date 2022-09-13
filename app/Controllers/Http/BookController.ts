ef2b2e954f53447ed1c6b5dad50229b3ea42ed4d
akandeseun <akandeseun44@gmail.com>
2022-09-13 19:05:20 +0100

    Book <--> Category relationship

--- app/Controllers/Http/BookController.ts
+++ app/Controllers/Http/BookController.ts
@@ -1,11 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CreateBookValidator from 'App/Validators/CreateBookValidator'
 import Book from 'App/Models/Book'
+import Category from 'App/Models/Category'
 
 export default class BookController {
   public async create({ request, response }: HttpContextContract) {
     const payload = await request.validate(CreateBookValidator)
     const book = await Book.create(payload)
     response.created(book)
+  }
+
+  public async addCategory({ params, request, response }: HttpContextContract) {
+    const book = await Book.findOrFail(params.id)
+    const category = await Category.findByOrFail('name', request.body().name)
+    await book.related('categories').attach([category.id])
+    return response.ok(book)
   }
 }
