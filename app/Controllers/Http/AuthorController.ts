de7686f085f55da5e74f94ef4ebc362d1aaf6b19
akandeseun <akandeseun44@gmail.com>
2022-09-15 12:59:36 +0100

    w.i.p: author logic

--- app/Controllers/Http/AuthorController.ts
+++ app/Controllers/Http/AuthorController.ts
@@ -1,8 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreateAuthorValidator from 'App/Validators/CreateAuthorValidator'
 import Author from 'App/Models/Author'
 import Category from 'App/Models/Category'
 
 export default class AuthorController {
+  public async create({ request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateAuthorValidator)
+    const author = await Author.create(payload)
+    return response.status(201).json({ author })
+  }
+
+  public async all({ response }: HttpContextContract) {
+    const authors = await Author.all()
+    return response.status(200).json({ authors })
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateAuthorValidator)
+    const author = await Author.findOrFail(params.id)
+    author.merge(payload)
+    await author.save()
+    return response.status(200).json({ author })
+  }
+
+  public async delete({ params, response }: HttpContextContract) {
+    const author = await Author.findOrFail(params.id)
+    await author.delete()
+    return response.status(200).json({ msg: 'author deleted' })
+  }
+
+  // To work on adding duplicate categories
   public async addCategory({ params, request, response }: HttpContextContract) {
     const author = await Author.findOrFail(params.authorId)
     const category = await Category.findByOrFail('name', request.body().name)
