9a468009ee5ee9c322ac1f93177f3f6492c894f8
akandeseun <akandeseun44@gmail.com>
2022-10-23 08:12:37 +0100

    permission migrations

---
+++ app/Models/Permission.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuidv4 } from 'uuid'
+
+export default class Permission extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column()
+  public routes: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  // Hooks
+  @beforeCreate()
+  public static assignId(permission: Permission) {
+    permission.id = uuidv4()
+  }
+}
